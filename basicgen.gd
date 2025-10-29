08f2851d93fb4521c59925c6663baf33b1df23b3
Andrew Pizano <apizan2@uic.edu>
2025-10-28 23:00:40 -0500

    basic hit detection

--- basicgen.gd
+++ basicgen.gd
@@ -12,13 +12,17 @@ func _ready() -> void:
 		plane = Plane(Vector3.UP, Vector3.ZERO)
 		gridmap_position = Vector3(round(root.position.x), 0, round(root.position.z))
 		heart.position = lerp(heart.position, gridmap_position, 1)
+
 func _process(_delta):
+	selector.get_node("Sprite3D").modulate = Color(1, 0, 0)
 	world_position = plane.intersects_ray(
 		view_camera.project_ray_origin(get_viewport().get_mouse_position()),
 		view_camera.project_ray_normal(get_viewport().get_mouse_position()))
 	if world_position != null:
 		gridmap_position = Vector3(round(world_position.x), 0, round(world_position.z))
 		selector.position = lerp(selector.position, gridmap_position, 1)
+		if(selector.get_node("RayCast3D").is_colliding()):
+			selector.get_node("Sprite3D").modulate = Color(1, 1, 1)
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not previous_state and world_position!=null:
-			print((heart.position-gridmap_position).abs())
+			print((selector.position).abs())
 		previous_state = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
