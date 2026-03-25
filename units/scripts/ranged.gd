983480a557e7cdb6f8fc3158f0b40c16bf7309cb
Andrew Pizano <apizan2@uic.edu>
2026-03-25 17:48:54 -0500

    basic bow shooting, need to fix orientation of alot of things

--- units/scripts/ranged.gd
+++ units/scripts/ranged.gd
@@ -5,15 +5,65 @@ extends Node
 @onready var speed := 20
 @export var damage := 5
 @export var apValue := 0 
-func _ready() -> void:
-	shoot()
+@onready var autoCombat = false
+@onready var attackRange = $"../AttackRange"
+@onready var unitController = $".."
+@onready var animationController = $"../animator"
+@onready var movementController = $"../movement"
+@onready var inRange ={}
+@onready var attackInRange = {}
+@onready var closest
+@onready var attacking = false
+
 func shoot():
+
 	var instance = rangedAttack.instantiate()
 	get_tree().current_scene.add_child(instance)
 	instance.damage = damage
 	instance.apValue = apValue
 	instance.global_position = projSpawn.global_position
 	instance.add_collision_exception_with(get_parent())
-	instance.linear_velocity=initial_direction * speed
+	
+	instance.linear_velocity=-projSpawn.global_transform.basis.z * speed
 	await get_tree().create_timer(2.0).timeout
-	shoot()
+	#shoot()
+func get_closest()->Node3D:
+	var currDist = 0
+	var dist = 0
+	var closetNode
+	for node in attackInRange:
+		currDist = get_parent().global_position.distance_squared_to(node.global_position)
+		#put a check here latter for friendly
+		if(currDist<dist||dist == 0):
+			dist=currDist
+			closetNode = node
+	return closetNode
+@onready var print_timer := 0.0
+func attack(body:Node3D):
+	closest = get_closest()
+	#idk if I need to do this but uh better safe then sorry
+	if(is_instance_valid(body)):
+		
+		animationController.start_attack()
+		shoot()
+		await get_tree().create_timer(get_parent().attackRate).timeout
+	##broken currently
+	if(closest!=null):
+		projSpawn.look_at(closest.global_transform.origin, Vector3.UP)
+		attack(closest)
+	else:
+		attacking = false
+func _physics_process(_delta):
+	if(attackInRange.size()>0&&!attacking):
+		attacking = true
+		attack(get_closest())
+func _on_attack_range_body_entered(body: Node3D) -> void:
+	if body == get_parent():
+		return
+	if body.get("faction")==null:
+		return
+	if unitController.friendly.has(body.faction):
+		return 
+	attackInRange[body] = ""
+func _on_attack_range_body_exited(body: Node3D) -> void:
+	attackInRange.erase(body)
